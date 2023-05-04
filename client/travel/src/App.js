9eb8bc4f367833ba90a10bce46f7907db1e3786f
Manoj Kumar <manojkp124@gmail.com>
2023-05-04 06:21:30 +0530

    updated packages and App.js component.

--- client/travel/src/App.js
+++ client/travel/src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
 import * as React from 'react';
-import Map from './Map';
 
 
 function App() {
-  
-  const pushPins = [
-    {
-      location: [47.6097, -122.3331],
-      option: { color: 'red' },
-    },
-    {
-      location: [47.6, -122.3],
-      option: { color: 'blue' },
-    },
-  ];
-
   return (
     <div>
-      <Map pushPins={pushPins}/>
+      <p>Hello Maps</p>
     </div>
   );
 }
